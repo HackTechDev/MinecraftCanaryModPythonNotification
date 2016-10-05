70630a8cdec2402f9c1e3bd00066b39e6d31b78b
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2016-10-05 22:11:32 +0200

    Fix tab

--- CanaryMod/McPi/src/mcpi/McPi.java
+++ CanaryMod/McPi/src/mcpi/McPi.java
@@ -12,82 +12,82 @@ import net.canarymod.api.entity.living.humanoid.Player;
 
 public class McPi extends Plugin implements CommandListener {
 
-	public static Logman logger;
+    public static Logman logger;
 
-	public McPi() { 
-		logger = getLogman();
-	}
+    public McPi() { 
+        logger = getLogman();
+    }
 
-  	private void usage(Player me) {
-    	me.chat("Usage: mcpi create <structure name>");
-  	}
+    private void usage(Player me) {
+        me.chat("Usage: mcpi create <structure name>");
+    }
 
-  	private void parseArgs(Player me, String [] args) {    
-    	if (args.length < 3) {
-      		usage(me);
-      		return;
-    	}
-    	if (args[1].equalsIgnoreCase("create")) {
-    	    try {
-      		    buildStructure(me, args);
-      	    } catch (Exception ex) {
-   			ex.printStackTrace();
-   		    }
-    	}
+    private void parseArgs(Player me, String [] args) {    
+        if (args.length < 3) {
+            usage(me);
+            return;
+        }
+        if (args[1].equalsIgnoreCase("create")) {
+            try {
+                buildStructure(me, args);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
 
-  	}
+    }
 
-  	private void buildStructure(Player me, String [] args)  throws IOException {
+    private void buildStructure(Player me, String [] args)  throws IOException {
 
-    	try {
-			//String cmd = "ls -al";
-			String cmd = "./McPi/mcpi.py " + args[1] + " " + args[2];
+        try {
+            //String cmd = "ls -al";
+            String cmd = "./McPi/mcpi.py " + args[1] + " " + args[2];
 
-			Runtime run = Runtime.getRuntime();
-			Process pr = run.exec(cmd);
-			pr.waitFor();
+            Runtime run = Runtime.getRuntime();
+            Process pr = run.exec(cmd);
+            pr.waitFor();
 
-			BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+            BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
 
-			String line = "";
-			while ((line=buf.readLine())!=null) {
-				System.out.println(line);
-			}
-		} catch (Exception ex) {
-   			ex.printStackTrace();
-   		}
-  	}
+            String line = "";
+            while ((line=buf.readLine())!=null) {
+                System.out.println(line);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
 
 
-	@Override
-		public boolean enable() { 
-			logger.info("Starting up");   
-			try {
-				Canary.commands().registerCommands(this, this, false);
-			} catch (CommandDependencyException e) {
-				logger.error("Duplicate command name");
-			}
-			return true;
-		}
+    @Override
+    public boolean enable() { 
+        logger.info("Starting up");   
+        try {
+            Canary.commands().registerCommands(this, this, false);
+        } catch (CommandDependencyException e) {
+            logger.error("Duplicate command name");
+        }
+        return true;
+    }
 
-	@Override
-		public void disable() {
-		}
+    @Override
+    public void disable() {
+    }
 
-	@Command(aliases = { "mcpi" },
-			description = "Execute Python command",
-			permissions = { "" },
-			toolTip = "/mcpi")
+    @Command(aliases = { "mcpi" },
+            description = "Execute Python command",
+            permissions = { "" },
+            toolTip = "/mcpi")
 
-		public void mcpiCommand(MessageReceiver caller, String[] args) {
-    		if (caller instanceof Player) { 
-      			Player me = (Player)caller; 
-      			parseArgs(me, args);
-    		}
+        public void mcpiCommand(MessageReceiver caller, String[] args) {
+            if (caller instanceof Player) { 
+                Player me = (Player)caller; 
+                parseArgs(me, args);
+            }
 
-			String msg = "Call McPi";
-			Canary.instance().getServer().broadcastMessage(msg);
-		}
+            String msg = "Call McPi";
+            Canary.instance().getServer().broadcastMessage(msg);
+        }
 
 }
